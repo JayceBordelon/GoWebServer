ef30de456f4a2871459bc15e5f37585e51098588
Jayce Bordelon <b.jayce@wustl.edu>
2025-04-03 08:51:06 -0500

    No need for comments

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -12,11 +12,9 @@ var (
 )
 
 func Init() {
-	// Set up log output to stdout (you could also log to a file)
 	infoHandle := os.Stdout
 	errorHandle := os.Stderr
 
-	// Define loggers
 	Info = log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Warn = log.New(infoHandle, "WARN: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Error = log.New(errorHandle, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
